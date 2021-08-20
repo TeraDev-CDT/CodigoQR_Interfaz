383e621dd620da33a7b253e06b21531556ea82e4
TeraDev-CDT <80227611+TeraDev-CDT@users.noreply.github.com>
2021-08-20 09:28:43 -0400

    Add files via upload

---
+++ Codigos_QR/Main.py
@@ -0,0 +1,152 @@
+
+# -> CDT-DEV <- {Rosnier Magallanes}#
+from tkinter import *
+
+
+#Prueba para retener informacion 
+
+def btn_clicked():
+    print("Button Clicked")
+    depart = path_entry.get()
+    equip = equip_entry.get()
+    marc = marc_entry.get()
+    state = state_entry.get()
+    print ("Tu Producto es")
+    print(depart + "-" + equip +"-" + marc + "-"+ state)
+
+
+
+#-----------------------------------
+window = Tk()
+window.title("Sistema Inventario")
+window.geometry("1000x600")
+window.iconbitmap("Icon.ico")
+#window.configure(bg = f"#7e24d8")
+
+
+
+canvas = Canvas(
+    window,
+    bg = "#7e24d8", #3D2FDF Azulado moradito
+    height = 600,
+    width = 1000,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge")
+#Imagen de Fondo 
+canvas.place(x = 0, y = 0)
+background_img = PhotoImage(file = f"background.png")
+background = canvas.create_image(
+    500, 299,
+    image=background_img)
+
+
+
+
+
+ #Texboxes1---------------------------------------------------
+path_entry_img = PhotoImage(file = f"img_textBox0.png")
+path_entry_bg = canvas.create_image(
+    238.0, 235.0, #Localization IMG X y Y = entra mas unidad, mas baja 
+    image = path_entry_img)
+
+path_entry =  Entry(
+    bd = 0,
+    bg = "#ececec",
+    highlightthickness = 0)
+path_entry.place(
+        x = 138.0, y = 225,
+        width = 200.0,
+        height = 28)
+
+canvas.create_text(
+    238.0, 195.0,
+    text = "Departamento",
+    fill = "#9e9e9e",
+    font = ("Roboto-Light", int(13.0)))
+
+#Texboxes2-----------------------------------------------------
+equip_entry_img = PhotoImage(file = f"img_textBox1.png")
+equip_entry_bg = canvas.create_image(
+    238.0, 315.0, #Localization IMG
+    image = equip_entry_img)
+
+equip_entry = Entry(
+    bd = 0,
+    bg = "#ececec",
+    highlightthickness = 0)
+
+equip_entry.place(
+    x = 138.0, y = 300.0,  #Localization Text BOX
+    width = 200.0,
+    height = 28)
+
+canvas.create_text(
+    238.0, 275.0, #Localization Text 
+    text = "Equipo",
+    fill = "#9e9e9e",
+    font = ("Roboto-Light", int(13.0)))
+
+#Texboxes3-----------------------------------------------------
+marc_entry_img = PhotoImage(file = f"img_textBox1.png")
+marc_entry_bg = canvas.create_image(
+    238.0, 400.0, #Localization IMG
+    image = marc_entry_img)
+
+marc_entry = Entry(
+    bd = 0,
+    bg = "#ececec",
+    highlightthickness = 0)
+
+marc_entry.place(
+    x = 138.0, y = 380.0,  #Localization Text BOX
+    width = 200.0,
+    height = 28)
+
+canvas.create_text(
+    238.0, 355.0, #Localization Text 
+    text = "Marca",
+    fill = "#9e9e9e",
+    font = ("Roboto-Light", int(13.0)))
+#Texboxes4-----------------------------------------------------
+state_entry_img = PhotoImage(file = f"img_textBox1.png")
+state_entry_bg = canvas.create_image(
+    238.0, 480.0, #Localization IMG
+    image = state_entry_img)
+
+state_entry = Entry(
+    bd = 0,
+    bg = "#ececec",
+    highlightthickness = 0)
+
+state_entry.place(
+    x = 138.0, y = 465.0,  #Localization Text BOX
+    width = 200.0,
+    height = 28)
+
+canvas.create_text(
+    238.0, 435.0, #Localization Text 
+    text = "Estado",
+    fill = "#9e9e9e",
+    font = ("Roboto-Light", int(13.0)))
+
+#Botones 
+img0 = PhotoImage(file = f"img0.png")
+b0 = Button(image=img0, borderwidth=0, highlightthickness=0, command=btn_clicked, relief="flat")
+
+b0.place(
+    x = 100, y = 510,
+    width = 142,
+    height = 48)
+
+#Botones 
+img1 = PhotoImage(file = f"img2.png")
+b1 = Button(image=img1, borderwidth=0, highlightthickness=0, command=btn_clicked, relief="flat")
+
+b1.place(
+    x = 250, y = 510,
+    width = 142,
+    height = 48)
+
+window.resizable(False, False)
+window.mainloop()
--- QR_interfaz.py
+++ QR_interfaz.py
@@ -1,7 +1,33 @@
 from tkinter import*
+from tkinter import messagebox
 import qrcode
 from PIL import Image,ImageTk
 from resizeimage import resizeimage
+from typing import Collection
+from pymongo import MongoClient, collection
+
+#Objeto codigo QR
+qr = qrcode.QRCode(
+	version=1,
+	error_correction=qrcode.constants.ERROR_CORRECT_H,
+	box_size=10,
+	border=4
+)
+
+#Conexiones a la base de datos
+
+mongo_conex = 'mongodb://localhost'
+mongo_puerto = "27017"
+mongo_cadena = mongo_conex+':'+mongo_puerto+'/'
+
+
+
+cliente = MongoClient(mongo_conex)
+cliente.server_info()
+print("Conexion exitosa a la base de datos")
+
+db = cliente['primerbase']
+coleccion = db['productos']
 
 
 class Qr_Generador:
@@ -11,7 +37,7 @@ class Qr_Generador:
         #Dimensiones Interfaz
         self.root.geometry("900x500+200+50")
         #Color Fondo Interfaz
-        root.configure(background='#51D1F6')
+        root.configure(background='#F6F3F1')
         
         #Titulo de la Interfaz Grafica
         self.root.title("Generador QR de Inventario")
@@ -22,12 +48,14 @@ class Qr_Generador:
         
         #Variables de entrada de los campos
         
-        self.var_emp_departamento = StringVar()
-        self.var_equipo = StringVar()
-        self.var_estado = StringVar()
+        self.var_emp_descripcion = StringVar()
         self.var_marca = StringVar()
+        self.var_modelo = StringVar()
+        self.var_cantidad = StringVar()
+        self.var_serial = StringVar()
+        self.var_COD_CDT = StringVar()
+        self.var_observacion = StringVar()
         
-    
         
         #Dimensiones de la seccion de los Datos del Producto
         
@@ -38,24 +66,32 @@ class Qr_Generador:
         
         emp_titulo = Label(emp_seccion,text="Datos del Producto",font=("arial",20),bg="#102C54",fg="white").place(x=0,y=0,relwidth=1)
         
-        etiqueta_emp_departamento = Label(emp_seccion,text="Departamento",font=("arial",15,"bold"),bg="white").place(x=20,y=60)
-        etiqueta_emp_equipo = Label(emp_seccion,text="Equipo",font=("arial",15,"bold"),bg="white").place(x=87,y=100)
-        etiqueta_emp_estado = Label(emp_seccion,text="Estado",font=("arial",15,"bold"),bg="white").place(x=87,y=140)
-        etiqueta_emp_marca = Label(emp_seccion,text="Marca",font=("arial",15,"bold"),bg="white").place(x=94,y=180)
+        etiqueta_emp_descripcion = Label(emp_seccion,text="DESCRIPCIÓN",font=("arial",10,"bold"),bg="white").place(x=120,y=60)
+        etiqueta_emp_marca = Label(emp_seccion,text="MARCA",font=("arial",10,"bold"),bg="white").place(x=165,y=90)
+        etiqueta_emp_modelo = Label(emp_seccion,text="MODELO",font=("arial",10,"bold"),bg="white").place(x=160,y=120)
+        etiqueta_emp_cantidad = Label(emp_seccion,text="CANTIDAD",font=("arial",10,"bold"),bg="white").place(x=150,y=150)
+        etiqueta_emp_serial = Label(emp_seccion,text="SERIAL",font=("arial",10,"bold"),bg="white").place(x=170,y=180)
+        etiqueta_emp_COD_CDT = Label(emp_seccion,text="COD_CDT",font=("arial",10,"bold"),bg="white").place(x=155,y=210)
+        etiqueta_emp_observaciones = Label(emp_seccion,text="OBSERVACIONES",font=("arial",10,"bold"),bg="white").place(x=102,y=240)
         
-        texto_emp_departamento = Entry(emp_seccion,font=("arial",14,"bold"),textvariable=self.var_emp_departamento,bg="#E4F4FD").place(x=200,y=60)
-        texto_emp_equipo = Entry(emp_seccion,font=("arial",14,"bold"),textvariable=self.var_equipo,bg="#E4F4FD").place(x=200,y=100)
-        texto_emp_estado = Entry(emp_seccion,font=("arial",14,"bold"),textvariable=self.var_estado,bg="#E4F4FD").place(x=200,y=140)
-        texto_emp_marca = Entry(emp_seccion,font=("arial",14,"bold"),textvariable=self.var_marca,bg="#E4F4FD").place(x=200,y=180)
+        texto_emp_descripcion = Entry(emp_seccion,font=("arial",10,"bold"),textvariable=self.var_emp_descripcion,bg="#E4F4FD").place(x=250,y=60)
+        texto_emp_marca = Entry(emp_seccion,font=("arial",10,"bold"),textvariable=self.var_marca,bg="#E4F4FD").place(x=250,y=90)
+        texto_emp_modelo = Entry(emp_seccion,font=("arial",10,"bold"),textvariable=self.var_modelo,bg="#E4F4FD").place(x=250,y=120)
+        texto_emp_cantidad = Entry(emp_seccion,font=("arial",10,"bold"),textvariable=self.var_cantidad,bg="#E4F4FD").place(x=250,y=150)
+        texto_emp_serial = Entry(emp_seccion,font=("arial",10,"bold"),textvariable=self.var_serial,bg="#E4F4FD").place(x=250,y=180)
+        texto_emp_COD_CDT = Entry(emp_seccion,font=("arial",10,"bold"),textvariable=self.var_COD_CDT,bg="#E4F4FD").place(x=250,y=210)
+        texto_emp_observaciones = Entry(emp_seccion,font=("arial",10,"bold"),textvariable=self.var_observacion,bg="#E4F4FD").place(x=250,y=240)
         
-        boton_registro = Button(emp_seccion,text="Registrar",font=("arial",18,"bold"),command=self.generar,bg="#102C54",fg="white").place(x=61,y=250,width=200,height=30)
-        boton_limpiar = Button(emp_seccion,text="Limpiar",command=self.limpiar,font=("arial",18,"bold"),bg="#9B9B9B",fg="white").place(x=310,y=250,width=120,height=30)
+        
+        
+        boton_registro = Button(emp_seccion,text="Registrar",font=("arial",12,"bold"),command=self.generar,bg="#102C54",fg="white").place(x=61,y=330,width=200,height=30)
+        boton_limpiar = Button(emp_seccion,text="Limpiar",command=self.limpiar,font=("arial",12,"bold"),bg="#9B9B9B",fg="white").place(x=310,y=330,width=120,height=30)
         
         #Mensaje Limpio al borrar campos
         
         self.mensaje = ""
         self.etiqueta_mensaje = Label(emp_seccion,text=self.mensaje,font=("arial",20,"bold"),bg="white",fg="#00BB2D")
-        self.etiqueta_mensaje.place(x=0,y=320,relwidth=1)
+        self.etiqueta_mensaje.place(x=0,y=280,relwidth=1)
         
         #Dimensiones de la seccion de Codigo QR
         
@@ -64,7 +100,7 @@ class Qr_Generador:
         
         #Ubicacion ruta de la imagen del logo del club
         
-        imagenlogo=PhotoImage("/home/jesus/Documentos/Programa/Python/Interfaz/logo.png")
+        #imagenlogo=PhotoImage("/home/jesus/Documentos/Programa/Python/Interfaz/logo.png")
         
         #Titulo Codigo QR
         
@@ -72,22 +108,25 @@ class Qr_Generador:
         
         #Imagen del Logo de Club arriba de Codigo QR
         
-        qr_logo = Label(qr_seccion,image=imagenlogo).place(x=75,y=50,width=100,height=100)
+        #qr_logo = Label(qr_seccion,image=imagenlogo).place(x=75,y=50,width=100,height=100)
     
         #Mensaje de Codigo QR Inactivo
         
         self.qr_codigo = Label(qr_seccion,text="Código QR\nInactivo",font=("arial",15),bg="#EEEEEE",fg="#DDDDDD",bd=1,relief=RIDGE)
-        self.qr_codigo.place(x=35,y=165,width=180,height=180)
+        self.qr_codigo.place(x=35,y=113,width=180,height=180)
      
      
      #Funcion que indica limpiar y/o borrar los campos de la GUI
      
     def limpiar(self):   
         
-        self.var_emp_departamento.set("")
-        self.var_equipo.set("")
-        self.var_estado.set("")
+        self.var_emp_descripcion.set("")
         self.var_marca.set("")
+        self.var_modelo.set("")
+        self.var_cantidad.set("")
+        self.var_serial.set("")
+        self.var_COD_CDT.set("")
+        self.var_observacion.set("")
         self.mensaje = ""
         self.etiqueta_mensaje.config(text=self.mensaje)
         self.qr_codigo.config(image="")
@@ -98,27 +137,73 @@ class Qr_Generador:
         
         #Declaracion de variables auxiliares de variables con objetos
         
-        c1 = self.var_emp_departamento
-        c2 = self.var_equipo
-        c3 = self.var_estado
-        c4 = self.var_marca
+        c1 = self.var_emp_descripcion.get()
+        c2 = self.var_marca.get()
+        c3 = self.var_modelo.get()
+        c5 = self.var_serial.get()
+        
+        
+        #Definicion de la variable cantidad bajo la condicion del serial
+        if c5=="N/T" or c5 =="S/D":
+            c4 = self.var_cantidad.get() 
+        else:
+            c4 = self.var_cantidad.set("1")
+            
+        c6 = self.var_COD_CDT.get()
+        c7 = self.var_observacion.get()
+        
+        
         
         #Condicion para generar Codigo QR al registrar datos
-        
-        if c1.get()=="" or c2.get()=="" or c3.get()=="" or c4.get() == "":
+        if c1=="" or c2== "" or c3== "" or c4 == "" or c5== "" or c6== "" or c7== "":
             self.mensaje = "Complete los campos correctamente!!!"
             self.etiqueta_mensaje.config(text=self.mensaje,fg="red")
         else:
-            qr_datos=(f"Departamento:{self.var_emp_departamento.get()}\nEquipo:{self.var_equipo.get()}\nEstado:{self.var_estado.get()}\nMarca:{self.var_marca.get()}")
-            qr_code=qrcode.make(qr_datos)
-            #print(qr_code)
-            qr_code=resizeimage.resize_cover(qr_code,[180,180])
-            qr_code.save("./"+str(self.var_emp_departamento.get())+".png")
             
-            self.im=ImageTk.PhotoImage(file="./"+str(self.var_emp_departamento.get())+".png")
+            #Definir un documento por el cual seran registrados los datos 
+            # que se ingresen en el programa con sus respectivos campos
+            
+            link = {
+                
+                "DESCRIPCION": self.var_emp_descripcion.get(),
+                "MARCA": self.var_marca.get(),
+                "MODELO": self.var_modelo.get(),
+                "CANTIDAD": self.var_cantidad.get(),
+                "SERIAL": self.var_serial.get(),
+                "COD_CDT": self.var_COD_CDT.get(),
+                "OBSERVACIONES": self.var_observacion.get()
+                
+            }
+            
+            #Agregar datos a la BD Inventario
+            coleccion.insert_one(link)
+            qr.add_data(link)
+            
+            #Contador/Iterados de productos del inventario
+            contador = coleccion.count_documents({"DESCRIPCION": self.var_emp_descripcion.get()})
+            print(self.var_emp_descripcion.get()+" --> "+"{}".format(contador))
+            
+            #Informacion que va a mostrar el QR
+            qr_datos=(f"DESCRIPCIÓN:{self.var_emp_descripcion.get()}\nMARCA:{self.var_marca.get()}\nMODELO:{self.var_modelo.get()}\nCANTIDAD:{self.var_cantidad.get()}\nSERIAL:{self.var_serial.get()}\nCOD_CDT:{self.var_COD_CDT.get()}\nOBSERVACIONES:{self.var_observacion.get()}")
+            qr_code=qrcode.make(qr_datos)
+            
+            #Dimensiones de la imagen de QR que se muestra en el programa
+            qr_code=resizeimage.resize_cover(qr_code,[180,180])
+            
+            #Definir el nombre del formato de imagen del codigo QR con el 
+            # identificador codigo que se le asigna al producto registrado
+            qr_code.save("./"+self.var_COD_CDT.get()+".png")
+            
+            #Definir el nombre del codigo QR del producto registrado va a ser guardado
+            self.im=ImageTk.PhotoImage(file="./"+str(self.var_COD_CDT.get())+".png")
+            
+            #Establecer el formato imagen del codigo QR por el objeto instanciado de la imagen
             self.qr_codigo.config(image=self.im)
+            
+            #Mostrar mensaje de aviso al generar el codigo QR y registro del producto
             self.mensaje = "QR Generado Exitosamente!!!"
             self.etiqueta_mensaje.config(text=self.mensaje,fg="#00BB2D")
+            
             
                           
 root = Tk()
